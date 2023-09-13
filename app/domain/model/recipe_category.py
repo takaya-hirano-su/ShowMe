8b0c6028468ac0f862a780d160707fef4c7f5679
Hirano2000 <takaya.hirano.mp1120@gmail.com>
2023-09-13 11:18:46 +0900

    add unique attribute to name column

--- app/domain/model/recipe_category.py
+++ app/domain/model/recipe_category.py
@@ -9,7 +9,7 @@ from infra.settings import Base
 class RecipeCategory(Base):
     __tablename__ = "recipe_categories"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    name = Column("name", String, nullable=False)
+    name = Column("name", String, unique=True, nullable=False)
 
     def __repr__(self):
         return f"<RecipeCategory(id={self.id}, name={self.name})>"
