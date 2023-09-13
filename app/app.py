9df6a6e798d82497572b9e9236dca9b8f942d4b3
shima004 <yellow030408@gmail.com>
2023-09-13 05:55:03 +0000

    feat: login endpoint

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 import framework.router.dishes as dishes_router
 import framework.router.food_categories as food_categories_router
 import framework.router.foods as foods_router
+import framework.router.login as login_router
 import framework.router.recipe_categories as recipe_categories_router
 import framework.router.recipes as recipes_router
 import framework.router.recipes_suggestions as recipes_suggestions_router
@@ -17,6 +18,7 @@ app.register_blueprint(dishes_router.dishes_router)
 app.register_blueprint(recipe_categories_router.recipe_categories_router)
 app.register_blueprint(recipes_router.recipes_router)
 app.register_blueprint(recipes_suggestions_router.recipes_suggestions_router)
+app.register_blueprint(login_router.login_router)
 
 
 @app.route("/")
