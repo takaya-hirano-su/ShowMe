a69f71f0807217be2c517edc68d5df69a675a778
hiroto-ogawa <hiroto.og12@gmail.com>
2023-09-13 11:52:11 +0900

    recipe,dishのモデル作成

---
+++ app/domain/model/recipe.py
@@ -0,0 +1,30 @@
+from uuid import uuid4
+
+from sqlalchemy import Column, ForeignKey, DateTime, String, Boolean
+from sqlalchemy.dialects.postgresql import UUID
+
+from infra.settings import Base
+
+
+class recipe(Base):
+    __tablename__ = "recipes"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    user_id = Column(UUID(as_uuid=True),ForeignKey('user.id'), nullable=False, default=uuid4)
+    recipe_category_id = Column(UUID(as_uuid=True),ForeignKey('recipe_category.id'), nullable=False, default=uuid4)
+    title = Column("title", String, nullable=False)
+    thumbnail_url = Column("thumbnail_url", String)
+    description = Column("description", String)
+    is_public = Column("is_public", Boolean, nullable=False)
+    is_draft = Column("is_public", Boolean, nullable=False)
+    created_at = Column("created_at", DateTime, server_default="now()")
+    updated_at = Column("updated_at", DateTime)
+    deleted_at = Column("deleted_at", DateTime)
+
+    def __repr__(self):
+        return f"<Recipe(id={self.id}, user_id={self.user_id}, recipe_category_id={self.recipe_category_id}, title={self.title}, thumbnail_url={self.thumbnail_url}, description={self.description}, is_public={self.is_public}, is_draft={self.is_draft}, created_at={self.created_at}, updated_at={self.updated_at}, deleted_at={self.deleted_at})>"
+
+
+if __name__ == "__main__":
+    from infra.settings import engine
+
+    Base.metadata.create_all(bind=engine)
