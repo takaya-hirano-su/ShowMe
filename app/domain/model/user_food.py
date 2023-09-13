ecfd2aff440d9fbe61d8170ed09b3f49bc2f801b
Hirano2000 <takaya.hirano.mp1120@gmail.com>
2023-09-13 11:58:39 +0900

    delete nullcable attribute of expired_at column

--- app/domain/model/user_food.py
+++ app/domain/model/user_food.py
@@ -12,11 +12,11 @@ class UserFood(Base):
     name = Column("name", String, nullable=False)
     user_id=Column("user_id",Integer,ForeignKey("user.id"))
     food_id=Column("food_id",Integer,ForeignKey("food.id"))
-    amount=Column("amount",Float,server_default=0,comment="単位無し. 数量のみ") 
+    amount=Column("amount",Float,server_default=0.0,comment="単位無し. 数量のみ") 
     deadline=Column("deadline", DateTime)
     created_at = Column("created_at", DateTime, server_default="now()")
     deleted_at = Column("deleted_at", DateTime)
-    expired_at = Column("expired_at", DateTime, nullable=False)
+    expired_at = Column("expired_at", DateTime)
     updated_at = Column("updated_at", DateTime)
 
     def __repr__(self):
