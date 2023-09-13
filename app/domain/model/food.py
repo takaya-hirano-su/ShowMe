18442a576fdaa8b3c4f9082aefef9d8db5d5e0ab
Hirano2000 <takaya.hirano.mp1120@gmail.com>
2023-09-13 11:39:39 +0900

    add food model

--- app/domain/model/food.py
+++ app/domain/model/food.py
@@ -1,6 +1,6 @@
 from uuid import uuid4
 
-from sqlalchemy import Column, DateTime, String,ForeignKey,Integer
+from sqlalchemy import Column, DateTime, String, ForeignKey, Integer
 from sqlalchemy.dialects.postgresql import UUID
 
 from infra.settings import Base
@@ -11,12 +11,13 @@ class Food(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     name = Column("name", String, nullable=False)
     icon_url = Column("icon_url", String)
-    food_category_id=Column("food_category_id",Integer,ForeignKey("food_category.id"))
-    deadline=Column("deadline", DateTime)
-
+    food_category_id = Column(
+        "food_category_id", Integer, ForeignKey("food_category.id")
+    )
+    deadline = Column("deadline", DateTime)
 
     def __repr__(self):
-        return f"<User(id={self.id}, name={self.name}, icon_url={self.icon_url}, mail_address={self.mail_address}, password_hash={self.password_hash}, created_at={self.created_at}, deleted_at={self.deleted_at})>"
+        return f"<Food(id={self.id}, name={self.name}, icon_url={self.icon_url}, food_category_id={self.food_category_id}, deadline={self.deadline})>"
 
 
 if __name__ == "__main__":
