f2bc98ac0d08694675fe0858738105856030bccc
Hirano2000 <takaya.hirano.mp1120@gmail.com>
2023-09-13 11:12:40 +0900

    add recipe_category

---
+++ app/domain/model/recipe_category.py
@@ -0,0 +1,27 @@
+# Table foods_recipes {
+#   recipe_id uuid [pk,ref: > recipes.id]
+#   food_id uuid [pk,ref: > foods.id]
+#   amount number
+# }
+
+from uuid import uuid4
+
+from sqlalchemy import Column, DateTime, String
+from sqlalchemy.dialects.postgresql import UUID
+
+from infra.settings import Base
+
+
+class RecipeCategory(Base):
+    __tablename__ = "recepi_categories"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    name = Column("name", String, nullable=False)
+
+    def __repr__(self):
+        return f"<RecipeCategory(id={self.id}, name={self.name})>"
+
+
+if __name__ == "__main__":
+    from infra.settings import engine
+
+    Base.metadata.create_all(bind=engine)
