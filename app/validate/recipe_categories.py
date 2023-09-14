b1754e043351414f8627baa94ca18dd1d7f3f1b2
shima004 <yellow030408@gmail.com>
2023-09-14 10:12:34 +0900

    remove: unused var

--- app/validate/recipe_categories.py
+++ app/validate/recipe_categories.py
@@ -5,11 +5,3 @@ register_recipe_categories_schema = {
     },
     "required": ["name"],
 }
-
-get_recipe_categories_schema = {
-    "type": "object",
-    "properties": {
-        "name": {"type": "string"},
-    },
-    "required": ["name"],
-}
